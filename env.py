19b04a7e0bd1612aafc00e42bf16f0da2d0fbe1f
seohyunwoo-0407 <shw4166@kaist.ac.kr>
2025-11-19 15:40:47 +0900

    env

---
+++ env.py
@@ -0,0 +1,127 @@
+import gymnasium as gym
+from gymnasium import spaces
+import yfinance as yf
+import numpy as np
+import datetime as dt
+import matplotlib.pyplot as plt
+from numpy._core.multiarray import dtype
+
+class TraidingEnv(gym.Env):
+    """
+    state 요소
+    1. 현재 잔고(balance)
+    2. 현재 보유 주식 수(shares)
+    3. 현재 포트폴리오 가치(total_value)
+    """
+    def __init__(
+        self,
+        balance: float = 10_000.0, #잔고
+        start_date: str = "2020-10-19",
+        end_date: str = "2024-10-25",
+        code: str = "AAPL"):
+
+        self.initial_balance = balance
+        self.start_date = dt.datetime.strptime(start_date, "%Y-%m-%d")
+        self.end_date = dt.datetime.strptime(end_date, "%Y-%m-%d")
+        self.code = code
+
+
+        # state/action 정의
+
+        self.observation_space = spaces.Box( # balance(잔고), shares(보유 주식 수), portfolio_value(현재 포트폴리오 가치)  Open(시가), High(당일 최고가), Low(당일 최저가), Close(종가), Volume(거래량)
+            low = np.array([0, 0, 0, -np.inf, -np.inf, -np.inf, -np.inf, -np.inf], dtype = np.float32),
+            high = np.array([np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf], dtype = np.float32),
+            shape = (8, ),
+            dtype = np.float32
+        )
+
+        #data 불러오기
+        ticker = yf.Ticker(code)
+        df = ticker.history(
+            interval = "1d",
+            start = self.start_date,
+            end = self.end_date,
+            auto_adjust = False
+            )
+        self.df = df
+
+        self.prices = df[["Open", "High", "Low", "Close", "Volume"]].to_numpy(dtype = np.float32)
+        self.n_steps = len(self.prices) # step 수
+
+        #0=sell, 1=hold, 2=buy
+        self.action_space = spaces.Discrete(3)
+
+        self.current_step = None #현재 step 저장
+        self.balance = None # 현재 잔고 저장
+        self.shares = None # 현재 보유 주식 수 저장
+        self.portfolio_value = None #현재 포트폴리오 가치 저장
+
+
+    def get_price(self):
+        """
+        거래는 항상 종가로 하니까 종가 출력하는 함수
+        """
+        return float(self.prices[self.current_step][3])
+
+    def get_observation(self):
+        """
+        현재 상태의 잔고, 보유 주식 수, 포트폴리오 가치, 시가, 당일 최고가, 당일 최저가, 종가, 거래량 한번에 출력
+        """
+        price_vec = self.prices[self.current_step]
+        obs = np.array([self.balance, self.shares, self.portfolio_value]+list(price_vec),
+                       dtype = np.float32,
+        )
+        return obs
+
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
+        self.current_step = 0
+        self.balance = float(self.initial_balance) # Modified: Use initial_balance directly
+        self.shares = 0.0
+
+        price = self.get_price()
+        self.portfolio_value = price * self.shares + self.balance
+        obs = self.get_observation()
+        info = {}
+        return obs, info
+
+    def step(self, action, count):
+        assert self.action_space.contains(action) #action이 0,1,2중 하나인지 확인
+
+        price = self.get_price()
+
+        prev_value = self.portfolio_value #이번스텝 시작할때 포트폴리오 가치
+
+        if action == 0: # sell
+            if self.shares >= count:
+                self.shares -= count
+                self.balance += price*count
+        elif action == 1: # hold
+            pass
+        elif action == 2: # buy
+            if self.balance >= price: # 잔고가 가격보다 크면
+                self.shares += count
+                self.balance -= price*count # 산만큼 잔고에서 뺌
+        else:
+            raise Exception("no valid action")
+
+        self.current_step += 1 #step 증가
+        if self.current_step >= self.n_steps:
+            done = True
+        else:
+            done = False
+        truncated = False #내가 임의로 조기 종료시킬때
+
+        price = self.get_price() #그 날의 종가
+        self.portfolio_value = self.balance + self.shares * price #포트폴리오 업데이트
+
+        reward = (self.portfolio_value - prev_value) / max(prev_value, 1.0)
+
+        obs = self.get_observation()
+        info = {
+            "portfolio_value": self.portfolio_value,
+            "balance": self.balance,
+            "shares": self.shares,
+            "action": action
+        }
+        return obs, reward, done, truncated, info
